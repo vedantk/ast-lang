98f5303212b9d44ea8b44a579fe86445eaed36f0
Vedant Kumar <vk@vedantk.com>
2012-01-28 14:59:56 -0800

    indentation fixes

--- test/realloc_check.scm
+++ test/realloc_check.scm
@@ -1,4 +1,4 @@
-(load "astl.scm")
+(load "../astl.scm")
 
 (def matcher
   (catch (FunctionDecl fdecl)
@@ -13,7 +13,7 @@
 
 (def (check-decl decl)
   (let* ((sub-cast (cast (child decl) CastExpr))
-  	 (target-buf (get-target-buf sub-cast)))
+         (target-buf (get-target-buf sub-cast)))
     (check-for-leak
       (: target-buf getFoundDecl)
       (cast (: decl getSingleDecl) NamedDecl) 
@@ -22,17 +22,17 @@
 (def (check-asgn asgn)
   (: asgn isAssignmentOp)
   (let* ((rhs (cast (: asgn getRHS) CastExpr)) 
-  	 (target-buf (get-target-buf rhs)))
+         (target-buf (get-target-buf rhs)))
     (check-for-leak
       (: target-buf getFoundDecl) 
       (: (cast (: asgn getLHS) DeclRefExpr) getFoundDecl)
       (: target-buf getLocation))))
 
 (def (fail-on-realloc call)
-  (if (get-realloc call)
-    (errs "On line "
-    	  (line-number (: call getRParenLoc))
-    	  ", the value returned by realloc() may be leaked.\n")))
+  (get-realloc call)
+  (errs "On line "
+        (line-number (: call getRParenLoc))
+        ", the value returned by realloc() may be leaked.\n"))
 
 (def (get-realloc call)
   (== "realloc"
@@ -41,7 +41,7 @@
 
 (def (get-target-buf sub-cast)
   (let* ((call (get-realloc (cast (child sub-cast) CallExpr)))
-  	 (wrap-cast (cast (: 0 => call getArg) CastExpr))
+     (wrap-cast (cast (: 0 => call getArg) CastExpr))
          (last-cast (cast (child wrap-cast) CastExpr)))
     (cast (child last-cast) DeclRefExpr)))
 
@@ -49,11 +49,11 @@
   (== (: lhs getQualifiedNameAsString)
       (: rhs getQualifiedNameAsString))
   (errs "On line "
-  	(line-number loc)
-  	", the original contents of "
-  	(: lhs getQualifiedNameAsString)
-  	" may be leaked.\n"))
+        (line-number loc)
+        ", the original contents of "
+        (: lhs getQualifiedNameAsString)
+        " may be leaked.\n"))
 
 (plugin "realloc-verifier"
-	"Check calls to realloc() for leaks."
-	matcher)
+  "Check calls to realloc() for leaks."
+  matcher)
